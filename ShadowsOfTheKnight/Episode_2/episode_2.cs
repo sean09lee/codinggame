4c3fa38bc120abcf60f689d3f45e69a224338105
Sean Lee <sean09lee@gmail.com>
2024-02-10 23:43:36 -0800

    Add more logic

--- ShadowsOfTheKnight/Episode_2/episode_2.cs
+++ ShadowsOfTheKnight/Episode_2/episode_2.cs
@@ -21,8 +21,8 @@ class Player
         // Define starting position
         inputs = Console.ReadLine().Split(' ');
         var position = new Position();
-        position.X = int.Parse(inputs[0]);
-        position.Y = int.Parse(inputs[1]);
+        position.X0, position.X = int.Parse(inputs[0]);
+        position.Y0, position.Y = int.Parse(inputs[1]);
 
         // game loop
         while (true)
@@ -55,20 +55,30 @@ class Player
 
     static void OnWarmer(Position position, Building building, string previousClue)
     {
-        if (position.X == position.X0)
+        if (previousClue == Clue.SAME)
         {
-            // last jump was vertical, so continue vertically jumping
+            // last jump was done to the correct position for a single axis
+            var xDiff = Math.Abs(position.X - position.X0);
+            if (xDiff > 0)
+            {
+                // last jump was horizontal, so jump vertically
+                JumpVertically(position, building);
+            }
+            else
+            {
+                // last jump was vertical, so jump horizontally
+                JumpHorizontally(position, building);
+            }
+        }
+        else if (position.X == position.X0 || position.X == building.Width || position.X == 0)
+        {
+            // last jump was vertical OR we have reached an edge, so jump vertically
             JumpVertically(position, building);
         }
         else
         {
             // last jump was horizontal, so continue jumping horizontally 
-            if (position.X == building.Width || position.X == 0)
-            {
-                // we have reached an edge, so start jumping vertically
-                JumpVertically(position, building);
-            }
-            else if (position.X > position.X0)
+            if (position.X > position.X0)
             {
                 // continue jumping right
                 JumpRight(position, building);
@@ -81,50 +91,105 @@ class Player
         }
     }
 
-    static void OnSame(Position position, Building building)
+    // TODO: finish logic
+    static void OnColder(Position position, Building building, string previousClue)
     {
+        switch (previousClue) {
+            case Clue.WARMER:
+                break;
+            case Clue.COLDER:
+                break;
+            case Clue.SAME:
+                break;
+            case Clue.UKNOWN:
+                // last x position was correct, so jump vertically
+                position.X = position.X0;
+                JumpVertically(position);
+                break;
+        }
+        if (previousClue == Clue.WARMER)
+        {
+            var xDiff = Math.Abs(position.X - position.X0);
+            var yDiff = Math.Abs(position.Y - position.Y0);
+            if (xDiff == 1)
+            {
+                // last x position was correct, so jump vertically
+                position.X = position.X0;
+                JumpVertically(position);
+            }
+            else if (xDiff > 1)
+            {
+                // difference is greater than 1, so retry horizontal jump
+                RetryHorizontalJump(position);
+            }
+            else if (yDiff == 1)
+            {
+                // last y position was correct, so jump horizontally
+            }
+            else if (yDiff > 1)
+            {
+                // difference is greater than 1, so retry vertical jump
+                RetryVerticalJump(position);
+            }
+            else
+            {
+                Console.Error.WriteLine(@$"Impossible scneario:
+                    X: {position.X}, X0: {position.X0}
+                    Y: {position.Y}, Y0: {position.Y0}");
+                throw;
+            }
+        }
+        else if(previousClue == Clue.COLDER)
+        {
 
+        }
+        else if()
+        {
+
+        }
     }
 
-    static void OnColder(Position position, Building building)
+    static void OnSame(Position position, Building building)
     {
-        if (position.X - position.X0 > 0)
+        // this means the correct position is the midpoint
+        var xDiff = Math.Abs(position.X - position.X0);
+        if (xDiff > 0)
         {
-            // last jump was right
-            if (previousClue == Clue.WARMER)
-            {
-
-            }
-        }
-        else if (position.X0 - position.X > 0)
-        {
-            // last jump was left
-            if (previousClue == Clue.WARMER)
-            {
-                
-            }
-        }
-        else if (position.Y - position.Y0 > 0)
-        {
-            // last jump was down
+            // last jump was horizontal
+            RetryHorizontalJump(position);
         }
         else
         {
-            // last jump was up
+            // last jump was vertical
+            RetryVerticalJump(position);
         }
     }
 
     static void JumpVertically(Position position, Building building)
     {
-        if (position.Y == 0 || position.Y < building.Height)
+        if (position.Y < building.Height)
         {
-            // we are either at the top floor OR we have not hit the bottom floor, so jump down
+            // we have not hit the bottom floor, so jump down
             JumpDown(position, building);
         }
         else
         {
-            // we are not at the bottom floor, so jump up
+            // we are at the bottom floor, so jump up
             JumpUp(position);
+        }
+    }
+
+    static void JumpHorizontally(Position position, Building building)
+    {
+        if (position.X < building.Width)
+        {
+            // we have not hit the right edge, so jump right
+            JumpRight(position, building);
+        }
+        else
+        {
+            // we are at the right edge, so jump left
+            JumpLeft(position);
         }
     }
 
@@ -170,6 +235,22 @@ class Player
         // set new current position
         var mid = (int)Math.Ceiling((decimal)position.X + building.Width / 2);
         position.X = mid;
+    }
+
+    static void RetryHorizontalJump(Position position)
+    {
+        // Note: do NOT reset previous turn variables as we are doing a retry
+        // set new current position halfway
+        var mid = (int)Math.Ceiling((decimal)position.X + position.X0 / 2);
+        position.X = mid;
+    }
+
+    static void RetryVerticalJump(Position position)
+    {
+        // Note: do NOT reset previous turn variables as we are doing a retry
+        // set new current position halfway
+        var mid = (int)Math.Ceiling((decimal)position.Y + position.Y0 / 2);
+        position.Y = mid;
     }
 }
 
